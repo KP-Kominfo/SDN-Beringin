47cb3e4b0e643983127fa894ba31e2a09c29b557
ardiansetya <ardiansetya129@gmail.com>
2025-06-27 14:24:28 +0700

    sejarah kurang foto

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -32,8 +32,8 @@
 
 
     <section class="max-w-7xl mx-auto px-4 py-12">
-        <div class="mb-10">
-          <h1 class="text-2xl md:text-4xl font-bold text-center text-gray-800">
+        <div class="mb-10 mx-auto text-center">
+          <h1 class="text-2xl md:text-4xl font-bold  text-gray-800 border-b-2 border-gray-300 inline-block pb-2">
             Sambutan Kepala Sekolah
           </h1>
         </div>
@@ -51,7 +51,7 @@
           
       
           <!-- Sambutan -->
-          <div class="bg-slate-100 border border-gray-400 p-6 rounded-lg shadow-md text-gray-800 text-justify  leading-relaxed space-y-4">
+          <div class="bg-gray-100 border border-gray-200 p-6 rounded-lg shadow-md text-gray-800 text-justify  leading-relaxed space-y-4">
             <p>
               Assalamualaikum warohmatullahi wabarokatuh.
               <br><br>
@@ -77,6 +77,70 @@
           </div>
         </div>
       </section>
+
+      {{-- sejarah sd --}}
+
+      <section class="max-w-7xl mx-auto px-4 py-12">
+        <div class="mb-10 mx-auto text-center">
+          <h1 class="text-2xl md:text-4xl font-bold text-center text-gray-800 border-b-2 border-gray-300 inline-block pb-2">
+            Sejarah SDN Bringin 01
+          </h1>
+        </div>
+      
+        <div class="flex flex-col md:flex-row gap-8">
+          <!-- Kolom Kiri -->
+          <div class="bg-gray-100 p-6 border border-gray-200 rounded-lg shadow-md text-gray-800 text-justify leading-relaxed space-y-4 flex-1">
+            <p>
+              Secara administrasi SD NEGERI BRINGIN 01 beralamat di Jalan Raya Gondoriyo, Kelurahan Bringin, Kecamatan Ngaliyan, Kota Semarang, Provinsi Jawa Tengah. Kode POS 50189. Dengan posisi geografis Lintang : -7 Bujur : 110
+            </p>
+            <p>
+              SD NEGERI BRINGIN 01 memiliki luas lahan 1.500 M², dengan batas-batas: Utara - Kelurahan Bringin, Timur - Kelurahan Kedungpane, Selatan - Kelurahan Wates, Barat - Kelurahan Gondoriyo.
+            </p>
+            <p>
+              Jarak tempuh ±15 menit dari Balaikota Semarang, SD ini terdiri dari 3 bangunan utama: 7 ruang kelas, 1 ruang kepala sekolah, 1 ruang guru, 1 ruang UKS, serta kantin dan toilet di bagian belakang.
+            </p>
+            <p>
+              SD NEGERI BRINGIN 01 berdiri pada 1 Januari 1979 melalui SK Pendirian No. 421.2/001/VI/32/85 dan SK Izin Operasional No. 421.2/04571/98 tanggal 24 Juli 1998. Informasi lengkap dapat dilihat di situs Data Pokok Pendidikan Kemendikbud.
+            </p>
+      
+            <div class="pt-4">
+              <p class="font-semibold">Kepala SDN Bringin 01</p>
+              <p>Muh Hasan Rifai, S.Pd.I, M.Pd</p>
+              <p class="text-sm text-gray-600">NIP: 19790507 200801 1 015</p>
+            </div>
+          </div>
+      
+          <!-- Kolom Kanan -->
+          <div class="flex-1 space-y-4 my-auto">
+            <!-- Gambar Gedung -->
+            <div class="w-full h-[300px] rounded-md overflow-hidden shadow-md">
+              <img
+                src="{{ asset('images/sejarah2.webp') }}"
+                alt="Foto Sekolah"
+                class="w-full h-full object-cover object-center"
+              />
+            </div>
+          
+            <!-- Box Informasi Visual -->
+            <div class="flex gap-4">
+              <div class="bg-gray-200 rounded-md h-[100px] w-1/2 flex items-center justify-center text-gray-600 shadow-sm">
+            
+              </div>
+              <div class="bg-gray-200 rounded-md h-[100px] w-1/2 flex items-center justify-center text-gray-600 shadow-sm">
+
+              </div>
+            </div>
+          
+            <!-- Kutipan -->
+            <div class="bg-gray-100 rounded-md p-4 text-sm italic text-gray-700 shadow-sm border-l-4 border-gray-400">
+              “SD NEGERI BRINGIN 01, BISA!!, "BERSIH, INDAH, SEHAT DAN ASRI”<br />
+              <span class="font-semibold text-gray-600">– SDN Bringin 01</span>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+      
       
 @endsection
 
